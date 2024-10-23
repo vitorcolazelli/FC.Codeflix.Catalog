cbde91448514bc49818f9e74b5a2f430d2a0e3e0
vitorcolazelli <vitorcolazelli@gmail.com>
2024-10-22 22:55:23 -0300

    creating seed works

---
+++ src/FC.Codeflix.Catalog.Domain/SeedWork/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FC.Codeflix.Catalog.Domain.SeedWork;
+
+public abstract class AggregateRoot : Entity
+{
+    protected AggregateRoot() : base() { }
+}
