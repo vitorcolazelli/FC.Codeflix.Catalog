328e8abbc9eb51e47165dd01971d4b31a3804898
vitorcolazelli <vitorcolazelli@gmail.com>
2024-10-26 00:36:06 -0300

    implementando feature de busca com filtros e paginação +testes

--- src/FC.Codeflix.Catalog.Domain/Repository/ICategoryRepository.cs
+++ src/FC.Codeflix.Catalog.Domain/Repository/ICategoryRepository.cs
@@ -1,8 +1,18 @@
 ﻿using FC.Codeflix.Catalog.Domain.Entity;
 using FC.Codeflix.Catalog.Domain.SeedWork;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
 
 namespace FC.Codeflix.Catalog.Domain.Repository;
 
-public interface ICategoryRepository : IGenericRepository<Category>
+public interface ICategoryRepository : 
+    IGenericRepository<Category>,
+    ISearchableRepository<Category>
 {
+    public Task<IReadOnlyList<Guid>> GetIdsListByIds(
+        List<Guid> ids,
+        CancellationToken cancellationToken);
+
+    public Task<IReadOnlyList<Category>> GetListByIds(
+        List<Guid> ids,
+        CancellationToken cancellationToken);
 }
---
+++ src/FC.Codeflix.Catalog.Domain/SeedWork/SearchableRepository/ISearchableRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+
+public interface ISearchableRepository<Taggregate>
+    where Taggregate : AggregateRoot
+{
+    Task<SearchOutput<Taggregate>> Search(
+        SearchInput input,
+        CancellationToken cancellationToken
+    );
+}
