f1704c68f838214c1468de776e265000acf11d41
vitorcolazelli <vitorcolazelli@gmail.com>
2024-10-31 15:58:16 -0300

    fix projects namespaces and implementing api and endtoendTests

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/Common/CategoryBaseFixture.cs
@@ -0,0 +1,76 @@
+using FC.Codeflix.Catalog.EndToEndTests.Base;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Category.Common;
+
+public class CategoryBaseFixture
+    : BaseFixture
+{
+    public CategoryPersistence Persistence;
+
+    public CategoryBaseFixture()
+        : base()
+    {
+        Persistence = new CategoryPersistence(
+            CreateDbContext()
+        );
+    }
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription =
+            Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+            categoryDescription =
+                categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean()
+        => new Random().NextDouble() < 0.5;
+
+    public string GetInvalidNameTooShort()
+        => Faker.Commerce.ProductName().Substring(0, 2);
+
+    public string GetInvalidNameTooLong()
+    {
+        var tooLongNameForCategory = Faker.Commerce.ProductName();
+        while (tooLongNameForCategory.Length <= 255)
+            tooLongNameForCategory = $"{tooLongNameForCategory} {Faker.Commerce.ProductName()}";
+        return tooLongNameForCategory;
+    }
+
+    public string GetInvalidDescriptionTooLong()
+    {
+        var tooLongDescriptionForCategory = Faker.Commerce.ProductDescription();
+        while (tooLongDescriptionForCategory.Length <= 10_000)
+            tooLongDescriptionForCategory = $"{tooLongDescriptionForCategory} {Faker.Commerce.ProductDescription()}";
+        return tooLongDescriptionForCategory;
+    }
+
+    public DomainEntity.Category GetExampleCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+
+    public List<DomainEntity.Category> GetExampleCategoriesList(int listLength = 15)
+        => Enumerable.Range(1, listLength).Select(
+            _ => new DomainEntity.Category(
+                GetValidCategoryName(),
+                GetValidCategoryDescription(),
+                GetRandomBoolean()
+            )
+        ).ToList();
+}
---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/CreateCategory/CreateCategoryApiTest.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Category.CreateCategory;
+
+[Collection(nameof(CreateCategoryApiTestFixture))]
+public class CreateCategoryApiTest
+{
+    private readonly CreateCategoryApiTestFixture _fixture;
+
+    public CreateCategoryApiTest(CreateCategoryApiTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(CreateCategory))]
+    [Trait("EndToEnd/API", "Category/Create - Endpoints")]
+    public async Task CreateCategory()
+    {
+        var input = _fixture.GetExampleInput();
+
+        var (response, output) = await _fixture.ApiClient
+            .Post<CategoryModelOutput>(
+                "/categories",
+                input
+            );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(HttpStatusCode.Created);
+        
+        output.Should().NotBeNull();
+        output!.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+        
+        var dbCategory = await _fixture.Persistence.GetById(output.Id);
+        
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be(input.IsActive);
+        dbCategory.Id.Should().NotBeEmpty();
+        dbCategory.CreatedAt.Should().NotBeSameDateAs(default);
+    }
+}
