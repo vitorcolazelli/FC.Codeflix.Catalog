92d6ab9ddd24ab9e414754b6f25039304d20b812
vitorcolazelli <vitorcolazelli@gmail.com>
2024-11-11 20:48:34 -0300

    implementando dominio genero

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTestFixture.cs
@@ -0,0 +1,31 @@
+using FC.Codeflix.Catalog.UnitTests.Common;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Genre;
+
+[CollectionDefinition(nameof(GenreTestFixture))]
+public class GenreTestFixtureCollection
+    : ICollectionFixture<GenreTestFixture>
+{
+}
+
+public class GenreTestFixture
+    : BaseFixture
+{
+    public string GetValidName()
+        => Faker.Commerce.Categories(1)[0];
+
+    public DomainEntity.Genre GetExampleGenre(
+        bool isActive = true,
+        List<Guid>? categoriesIdsList = null)
+    {
+        var genre = new DomainEntity.Genre(GetValidName(), isActive);
+        if (categoriesIdsList is null) 
+            return genre;
+        
+        foreach(var categoryId in categoriesIdsList)
+            genre.AddCategory(categoryId);
+        
+        return genre;
+    }
+}
