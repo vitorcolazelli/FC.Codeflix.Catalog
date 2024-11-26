c7840168543ad05330eaa6b35a65dcb8ff5c8b05
vitorcolazelli <vitorcolazelli@gmail.com>
2024-11-25 21:32:54 -0300

    delete genre feature

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Genre/DeleteGenre/DeleteGenreInput.cs
@@ -0,0 +1,12 @@
+using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre;
+
+public class DeleteGenreInput : IRequest
+{
+    public DeleteGenreInput(Guid id) 
+        => Id = id;
+
+    public Guid Id { get; set; }
+}
