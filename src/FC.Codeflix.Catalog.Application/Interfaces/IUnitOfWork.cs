22092f129b21dd58dbd57c10bdb8e8f00a75b37a
vitorcolazelli <vitorcolazelli@gmail.com>
2024-12-24 00:04:29 -0300

    remove usings

--- src/FC.Codeflix.Catalog.Application/Interfaces/IUnitOfWork.cs
+++ src/FC.Codeflix.Catalog.Application/Interfaces/IUnitOfWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FC.Codeflix.Catalog.Application.Interfaces;
+﻿namespace FC.Codeflix.Catalog.Application.Interfaces;
 
 public interface IUnitOfWork
 {
